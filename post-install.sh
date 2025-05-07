52643d7cfd95c398ce3f0dd40db27f6a9df3d2f9
Denis Pollini <denis.pollini.90@gmail.com>
2025-05-07 19:32:19 +0200

    edit software.sh

--- post-install.sh
+++ post-install.sh
@@ -125,8 +125,8 @@ echo
 yes | sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 yes | sudo pacman-key --lsign-key 3056513887B78AEB
 
-sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 
-sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+yes | sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 
+yes | sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
 
 
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
