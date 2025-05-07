3a69f14abcdd56bb59c614bcef93e9fe5a4007e8
Denis Pollini <denis.pollini.90@gmail.com>
2025-05-07 18:28:26 +0200

    edit script

---
+++ change-shell-to-fish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo
+tput setaf 3
+echo "######################################################"
+echo "################### Change shell to Fish"
+echo "######################################################"
+tput sgr0
+echo
+# Change shell to Fish
+
+chsh -s /usr/bin/fish $USER
+
+
+
+
