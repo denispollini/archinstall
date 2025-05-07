b84e0daff324b17f88164dcb9eb5636e4c2fd0c5
Denis Pollini <denis.pollini.90@gmail.com>
2025-05-07 19:29:07 +0200

    edit software

--- post-install.sh
+++ post-install.sh
@@ -122,8 +122,8 @@ tput sgr0
 echo
 # Install Chaotic Repo
 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com --batch
-sudo pacman-key --lsign-key 3056513887B78AEB --batch
+yes | sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+yes | sudo pacman-key --lsign-key 3056513887B78AEB
 
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
